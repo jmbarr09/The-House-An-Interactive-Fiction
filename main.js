b28e9fec9d9f75dbabd0357ed1a6a93736cff0ce
jmbarr09 <47217533+jmbarr09@users.noreply.github.com>
2019-05-14 13:50:35 -0400

    fixi boi

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
-
 // Create the contents of your room here.
 alert("Loading main.js!"); //Don't change this line
 player = new Player()
 
 //Create your objects
-let hallway = new Room("dusty hallway", "clouds of dust kick up with every step.");
-let vase = new Item("vase", "made of blue glass, chipped on top. Filled with a dark liquid.")
-let room = new Room("dark room", "It is dark");
+let foyer = new Room("foyer", "clouds of dust kick up with every step.");
+let table = new Item("table", "made of clear glass, chipped on top. Surrounded by chairs.")
+let  livingroom = new Room("living room", "Large dusty couch and coffee table infront");
+let chairs = new Item("chairs", "Three small polished wooden chairs, with a small gold key laid on one")
+let couch = new Item("couch", "an L shaped couch with grey seats with rips in almost all of them, nothing useful")
+let diningroom = new Room("dining room", "theres a marble bar table with four stools around it, theres ")
 
-hallway.addItem(vase);
-hallway.addItem(room);
+// Put them in their spots
 
-player.location = hallway;
+foyer.addItem(table);
+foyer.addItem(livingroom);
+livingroom.addItem(chairs);
+livingroom.addItem(couch);
+foyer.addItem(diningroom);
+player.location = foyer
+
 player.location.enter();
